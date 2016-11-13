9cb8ee11b208d0d48b8bc9c0019d3c01a16cef52
adlly <mm@ifnot.cc>
2016-11-13 21:09:59 +0800

    图片请求

--- app/src/main/java/lly/test/Main24Activity.java
+++ app/src/main/java/lly/test/Main24Activity.java
@@ -2,16 +2,20 @@ package lly.test;
 
 import android.app.Application;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import lly.test.Net.Net;
@@ -21,6 +25,9 @@ import static com.android.volley.Request.Method.GET;
 public class Main24Activity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv;
+    private ImageView iv;
+    private String main24 = "Main24";
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +40,9 @@ public class Main24Activity extends AppCompatActivity implements View.OnClickLis
     private void initView() {
        findViewById(R.id.btn).setOnClickListener(this);
         findViewById(R.id.nnew).setOnClickListener(this);
+        findViewById(R.id.btn_req_img).setOnClickListener(this);
         tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
     }
 
     @Override
@@ -45,7 +54,7 @@ public class Main24Activity extends AppCompatActivity implements View.OnClickLis
 //                获得requestQueue
 //                RequestQueue requestQueue = App.getRequestQueue();
 
-                RequestQueue requestQueue = Net.getInstance(getApplicationContext()).getRequestQueue();
+                requestQueue = Net.getInstance(getApplicationContext()).getRequestQueue();
 
                 String url = "http://httpbin.org/get";
                 Response.Listener<String> listener = new Response.Listener<String>() {
@@ -73,8 +82,40 @@ public class Main24Activity extends AppCompatActivity implements View.OnClickLis
                 startActivity(intent);
 
                 break;
+            case R.id.btn_req_img:
+                requestQueue = Net.getInstance(getApplicationContext()).getRequestQueue();
+
+                url = "http://pic33.nipic.com/20130916/3420027_192919547000_2.jpg";
+
+                Bitmap.Config decodeConfig = Bitmap.Config.ARGB_8888;
+
+                Response.Listener<Bitmap> listener_img = new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        iv.setImageBitmap(response);
+                    }
+                };
+                Response.ErrorListener errorListener_img = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        iv.setImageResource(R.mipmap.ic_launcher);
+                        error.printStackTrace();
+                    }
+                };
+                ImageRequest iq = new ImageRequest(url, listener_img, 0, 0, ImageView.ScaleType.FIT_CENTER, decodeConfig, errorListener_img);
+
+                iq.setTag(main24);
+                requestQueue.add(iq);
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(requestQueue != null) {
+            requestQueue.cancelAll(main24);
         }
     }
 }
