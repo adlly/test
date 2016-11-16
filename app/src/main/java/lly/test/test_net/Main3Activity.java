aa1158011ab708d76ac5de58d57a643bff04f7d3
huaixiaoz <hello@itmp.top>
2016-11-17 00:09:06 +0800

    fix file.getName() bug

--- app/src/main/java/lly/test/test_net/Main3Activity.java
+++ app/src/main/java/lly/test/test_net/Main3Activity.java
@@ -2,26 +2,23 @@ package lly.test.test_net;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import lly.test.R;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+
+import lly.test.R;
 
 
 public class Main3Activity extends AppCompatActivity {
@@ -80,7 +77,16 @@ public class Main3Activity extends AppCompatActivity {
                                     fileOutputStream.write(bytes, 0, len);
                                 }
                                 fileOutputStream.close();
-                                Bitmap bitmap = BitmapFactory.decodeFile(file.getName());
+
+                                /**
+                                 * file.getName() 只是获得文件名字, 不是文件路径 所以报错,
+                                 * 应该仔细看看报错信息
+                                 * 11-17 00:01:00.017 31222-1554/lly.test E/BitmapFactory: Unable to decode stream: java.io.FileNotFoundException: u=949232872,2013536703&fm=11&gp=0.jpg: open failed: ENOENT (No such file or directory)
+                                 * 如果实在找不到问题所在, 首先可以判断是肯定是文件 not found
+                                 * 那就把 file.getName() 变量打印出来看一下
+                                 * */
+                                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                                Log.e("getName", file.getName());
                                 Message msg = handler.obtainMessage();
                                 msg.what = IMAGE_VIEW;
                                 msg.obj = bitmap;
