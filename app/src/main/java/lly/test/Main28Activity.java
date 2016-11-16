ed46a494d145db6c61ece39e3c65286d0d683fa4
ad <ad@itmp.top>
2016-11-16 11:13:59 +0800

    error

--- app/src/main/java/lly/test/Main28Activity.java
+++ app/src/main/java/lly/test/Main28Activity.java
@@ -3,28 +3,54 @@ package lly.test;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class Main28Activity extends AppCompatActivity {
 
-    private ImageView iv;
+    static private ImageView iv;
     private Task_Asy task_asy;
+    final static int IMAGE_VIEW = 0x11;
+    final static int TOAST_TEXT = 0x10;
+    static Main28Activity ma;
+
+    static Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case IMAGE_VIEW:
+                    Bitmap bitmap = (Bitmap)msg.obj;
+                    iv.setImageBitmap(bitmap);
+                    break;
+                case TOAST_TEXT:
+                    Toast.makeText(ma, "下载失败!", Toast.LENGTH_SHORT).show();
+                default:
+                    break;
+            }
+        }
+    };
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main26);
+        ma = this;
 
         initView();
     }
@@ -48,28 +74,50 @@ public class Main28Activity extends AppCompatActivity {
         @Override
         protected Bitmap doInBackground(String... strings) {
 
-            try {
-                URL url = new URL(strings[0]);
-                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setConnectTimeout(5000);
-                urlConnection.setReadTimeout(5000);
-                urlConnection.connect();
 
-                if(urlConnection.getResponseCode() == 200){
-                    InputStream inputStream = urlConnection.getInputStream();
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-
-                    return bitmap;
-                }
+            final String ss = strings[0];
 
 
+                new Thread(){
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                    @Override
+                    public void run() {
+
+                        URL url = null;
+                        try {
+                            url = new URL(ss);
+                            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                            urlConnection.setRequestMethod("GET");
+                            urlConnection.setConnectTimeout(5000);
+                            urlConnection.setReadTimeout(5000);
+                            urlConnection.connect();
+
+
+                            if(urlConnection.getResponseCode() == 200){
+                                InputStream inputStream = urlConnection.getInputStream();
+                                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                Message msg = new Message();
+                                msg.what = IMAGE_VIEW;
+                                msg.obj = bitmap;
+                            }
+                            else{
+                                Message msg = handler.obtainMessage();
+                                msg.what = TOAST_TEXT;
+
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }.start();
+
+
+
             return null;
         }
 
